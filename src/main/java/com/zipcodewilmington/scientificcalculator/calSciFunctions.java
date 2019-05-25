8d22b49b1675a50ae3874ccd1d13a7ed05030024
Caleb Powell <calebpowell@zipcoders-MacBook-Pro.local>
2019-05-25 01:32:20 -0400

    Made stub methods in calSciFunctions and calCoreFunctions classes

--- src/main/java/com/zipcodewilmington/scientificcalculator/calSciFunctions.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/calSciFunctions.java
@@ -13,7 +13,7 @@ public class calSciFunctions {
     public calSciFunctions() {
         memoryVal  = new Double(0);
         currentVal = new Double(0);
-        currentUnitMode = DEG;
+        currentUnitMode = UnitMode.DEG;
     }
 
     // Trig Functions
@@ -48,6 +48,39 @@ public class calSciFunctions {
     }
 
 
+
+
+    // user memory management
+    public void clearMemory(){
+//        clear mem
+    }
+
+    public void mPlus(Double disp){
+//        add to memVal
+    }
+
+    public Double getMemory(){
+//        give back current memVal
+        return 0d;
+    }
+
+
+    // logarithmic functions
+    public Double log(Double a){
+        return 0d;
+    }
+
+    public Double inverseLog(Double a){
+        return 0d;
+    }
+
+    public Double naturalLog(Double a){
+        return 0d;
+    }
+
+    public Double inverseNatLog(Double a){
+        return 0d;
+    }
 
 
 
